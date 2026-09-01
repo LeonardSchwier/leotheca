3f5610a5a12f19581176e903adc1e665dcbd920c
Leonard Schwier <github@leonardschwier.de>
2026-09-01 20:56:53 +0200

    Fix MainActivity destroy override visibility

--- android/app/src/main/java/com/leonardschwier/leotheca/MainActivity.java
+++ android/app/src/main/java/com/leonardschwier/leotheca/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         if (newNoteTrackerExecutor != null) {
             newNoteTrackerExecutor.shutdownNow();
             newNoteTrackerExecutor = null;
